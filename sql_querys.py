0f6160d1eef4d642476a6c5460980192a9ce1bc7
TamaraSavadyan <savadyan.tamara@gmail.com>
2023-02-24 12:50:30 +0300

    solved

--- sql_querys.py
+++ sql_querys.py
@@ -1,8 +1,7 @@
 # INFO
 # Вывести топ 5 самых коротких по длительности перелетов.  Duration - разница между scheduled_arrival и scheduled_departure.
 # В ответе должно быть 2 колонки [flight_no, duration]
-TASK_1_QUERY = """
-"""
+TASK_1_QUERY = """SELECT flight_no, scheduled_arrival - scheduled_departure as duration from flights order by duration limit 5;"""
 #  flight_no | duration
 # -----------+----------
 #  PG0235    | 00:25:00
@@ -16,8 +15,7 @@ TASK_1_QUERY = """
 # Вывести топ 3 рейса по числу упоминаний в таблице flights
 # количество упоминаний которых меньше 50
 # В ответе должно быть 2 колонки [flight_no, count]
-TASK_2_QUERY = """
-"""
+TASK_2_QUERY = """SELECT flight_no, count(flight_no) as count FROM flights GROUP BY flight_no HAVING count(flight_no) < 50 ORDER BY count DESC LIMIT 3;"""
 #  flight_no | count
 # -----------+-------
 #  PG0260    |    27
@@ -27,8 +25,7 @@ TASK_2_QUERY = """
 # INFO
 # Вывести число перелетов внутри одной таймзоны
 # Нужно вывести 1 значение в колонке count
-TASK_3_QUERY = """
-"""
+TASK_3_QUERY = """SELECT count(1) FROM flights AS fl JOIN airports_data AS air_a ON fl.arrival_airport = air_a.airport_code JOIN airports_data AS air_b ON fl.departure_airport = air_b.airport_code WHERE air_a.timezone = air_b.timezone;"""
 #  count
 # --------
 #  16824
